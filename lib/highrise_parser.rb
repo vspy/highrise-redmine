4e25c22eb66f43705e429a4846c6de62abbc48fa
Victor Bilyk <victorbilyk@gmail.com>
2011-07-13 14:04:32 +0700

    sample docs are parsed somehow

--- lib/highrise_parser.rb
+++ lib/highrise_parser.rb
@@ -2,11 +2,30 @@ require 'xmlsimple'
 
 class HighriseParser 
 
-  def parse(body) 
+  def parseCompanies(body) 
+    companies = []
+
+    doc = XmlSimple.xml_in(body)
+    doc['company'].each do |company|
+      companies << {
+        :id =>  company['id'][0]['content'],
+        :name => company['name'][0],
+      }
+    end
+
+    return companies
+  end
+
+  def parsePersons(body) 
     persons = []
 
     doc = XmlSimple.xml_in(body)
     doc['person'].each do |person|
+      persons << {
+        :id => person['id'][0]['content'],
+        :firstName => person['first-name'][0],
+        :lastName => person['last-name'][0],
+      }
     end
     
     return persons
--- spec/highrise_parser_spec.rb
+++ spec/highrise_parser_spec.rb
@@ -1,20 +1,35 @@
 # coding: utf-8
 require 'highrise_parser'
 
-def read
-  dir = File.dirname(__FILE__) 
-  file = File.open(dir+"/persons.xml")
-  content = file.read
-  h = HighriseParser.new
-  h.parse(content)
-end
-
 describe HighriseParser do
 
-  result = read
+  it "reads persons sample data correctly" do
+    dir = File.dirname(__FILE__) 
+    file = File.open(dir+"/persons.xml")
+    content = file.read
+    h = HighriseParser.new
+    result = h.parsePersons(content)
 
-  it "reads sample data" do
     result.length.should == 2
+    result[0][:id].should == "1"
+    result[1][:id].should == "2"
+    result[0][:firstName].should == "John"
+    result[1][:firstName].should == "Jane"
   end
+
+  it "reads companies sample data correctly" do
+    dir = File.dirname(__FILE__) 
+    file = File.open(dir+"/companies.xml")
+    content = file.read
+    h = HighriseParser.new
+    result = h.parseCompanies(content)
+
+    result.length.should == 2
+    result[0][:id].should == "1"
+    result[1][:id].should == "2"
+    result[0][:name].should == "Doe Inc."
+    result[1][:name].should == "Acme Inc."
+  end
+
 end
 
