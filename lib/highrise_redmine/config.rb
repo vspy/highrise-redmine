ff5f06b6a99f2834da1dce21f0ef81354c806bb9
Victor Bilyk <victorbilyk@gmail.com>
2011-07-15 13:33:37 +0700

    creating export task, small fix for config

--- lib/highrise_redmine/config.rb
+++ lib/highrise_redmine/config.rb
@@ -3,10 +3,12 @@ require 'yaml'
 class HighriseRedmine
 
   class Config
+    attr_accessor :src, :dst
+ 
     def initialize(filename)
-      @yaml = YAML.load_file( filename )
-
-      raise "No source url specified" if @yaml[:source].nil?
+      yaml = YAML.load_file( filename )
+      @src = yaml['source'] || yaml['src'] || (raise "No source url specified")
+      @dst = yaml['destination'] || yaml['dst'] || (raise "No destination url specified")
     end
   end
 
